8287d79403398e5d5fdc2bf947205e325c2a1335
Hogofwar <hogofwar25@gmail.com>
2013-03-22 20:31:34 +0000

    Removed useless imports.

--- src/NameGen.java
+++ src/NameGen.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
